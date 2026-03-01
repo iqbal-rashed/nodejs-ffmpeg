84f3ac9d6a8d4cf229ccdce83636011ab45e4d73
Rashed Iqbal <dev.rashediqbal@gmail.com>
2026-03-01 11:52:07 +0600

    update some code

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,21 +3,8 @@ import { defineConfig } from "tsup";
 export default defineConfig([
   {
     entry: ["src/index.ts"],
-    format: ["cjs"],
+    format: ["cjs", "esm"],
     dts: true,
-    splitting: false,
-    sourcemap: false,
-    clean: true,
-    treeshake: true,
-    shims: true,
-    minify: true,
-    target: "node18",
-    outDir: "dist",
-  },
-  {
-    entry: ["src/index.ts"],
-    format: ["esm"],
-    dts: false,
     splitting: false,
     sourcemap: false,
     clean: true,
