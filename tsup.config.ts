ccf0d0bed89e5578d5af662edb5cdb1528ab39a0
Rashed Iqbal <dev.rashediqbal@gmail.com>
2026-05-12 22:10:06 +0600

    update commonjs module

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,8 +3,26 @@ import { defineConfig } from "tsup";
 export default defineConfig([
   {
     entry: ["src/index.ts"],
-    format: ["cjs", "esm"],
+    format: ["esm"],
     dts: true,
+    splitting: false,
+    sourcemap: false,
+    clean: true,
+    treeshake: true,
+    shims: true,
+    minify: true,
+    target: "node18",
+    outDir: "dist",
+    outExtension({ format }) {
+      return {
+        js: format === "esm" ? ".mjs" : ".cjs",
+      };
+    },
+  },
+  {
+    entry: ["src/index.ts"],
+    format: ["cjs"],
+    dts: false,
     splitting: false,
     sourcemap: false,
     clean: true,
